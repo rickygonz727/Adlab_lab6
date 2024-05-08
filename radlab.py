50b18975cb6a307b0cb1bdc1de1574a495ad0d67
rickygonz727 <113153709+rickygonz727@users.noreply.github.com>
2024-05-08 11:33:23 -0400

    Final Updates

--- radlab.py
+++ radlab.py
@@ -1,7 +1,7 @@
 """Code for the Radiation Lab - Adlab Lab 6
 
 Authors: Ad-Lab 3 Spring 24 Semester
-Version" April 19th, 2024
+Version: May, 2024
 
 """
 
@@ -26,14 +26,6 @@ time8, counts8 = np.genfromtxt("rad_15cm.txt",skip_header=7, unpack=True)
 time9, counts9 = np.genfromtxt("rad_10cm.txt",skip_header=7, unpack=True)
 time10, counts10 = np.genfromtxt("rad_05cm.txt",skip_header=7, unpack=True)
 
-#This next chunk imports the code from the lego setup. 
-time1h, counts1h = np.genfromtxt("measurement_1_holedown.csv", delimiter=',', skip_header=1,unpack=True)
-time19h, counts19h = np.genfromtxt("measurement_19_holedown.csv", delimiter=',', skip_header=1,unpack=True)
-time37h, counts37h = np.genfromtxt("measurement_37_holedown.csv", delimiter=',', skip_header=1,unpack=True)
-time55h, counts55h = np.genfromtxt("measurement_55_holedown.csv", delimiter=',', skip_header=1,unpack=True)
-time69h, counts69h = np.genfromtxt("measurement_69_holedown.csv", delimiter=',', skip_header=1,unpack=True)
-time84h, counts84h = np.genfromtxt("measurement_84_holedown.csv", delimiter=',', skip_header=1,unpack=True)
-time98h, counts98h = np.genfromtxt("measurement_98_holedown.csv", delimiter=',', skip_header=1,unpack=True)
 
 #This imports the background radiation, which will be used to obtain the true counts for each trial. 
 timebg, countsbg = np.genfromtxt("background.csv",delimiter=',', skip_header=1, unpack=True)
@@ -60,20 +52,12 @@ if __name__ == "__main__":
     counts9 = fn.true_counts(counts9,countsbg)
     counts10 = fn.true_counts(counts10,countsbg)
     
-    counts1h = fn.true_counts(counts1h,countsbg)
-    counts19h = fn.true_counts(counts19h,countsbg)
-    counts37h = fn.true_counts(counts37h,countsbg)
-    counts55h = fn.true_counts(counts55h,countsbg)
-    counts69h = fn.true_counts(counts69h,countsbg)
-    counts84h = fn.true_counts(counts84h,countsbg)
-    counts98h = fn.true_counts(counts98h,countsbg)
-
     #%% Distance Array
     
     #With our counts-values organized, we then work towards building an array of distance values from the different radiation
     #measurements. These values are ordered from group 1 to group 2.
     
-    distance = np.zeros(17)
+    distance = np.zeros(10)
     distance[0] = 0.7
     distance[1] = 0.6
     distance[2] = 0.5
@@ -84,36 +68,22 @@ if __name__ == "__main__":
     distance[7] = 0.15
     distance[8] = 0.1
     distance[9] = 0.05
-    distance[10] = 0.792
-    distance[11] = 0.648
-    distance[12] = 0.504
-    distance[13] = 0.36
-    distance[14] = 0.248
-    distance[15] = 0.128
-    distance[16] = 0.016
 
     #%%Counts Array
     #This section creates an array for the sum of the counts for each interval while also returning the uncertainty
     #It creates a new array of to store the values that were created from the stats function
-    sum_counts = np.zeros(17)
+    sum_counts = np.zeros(10)
     
-    sum_counts[0] = fn.stats(counts1h,0.792,True)[1]
-    sum_counts[1] = fn.stats(counts1,0.7,True)[1]
-    sum_counts[2] = fn.stats(counts19h,0.648,True)[1]
-    sum_counts[3] = fn.stats(counts2,0.6,True)[1]
-    sum_counts[4] = fn.stats(counts37h,0.504,True)[1]
-    sum_counts[5] = fn.stats(counts3,0.5,True)[1]
-    sum_counts[6] = fn.stats(counts4,0.4,True)[1]
-    sum_counts[7] = fn.stats(counts55h,0.36,True)[1]
-    sum_counts[8] = fn.stats(counts5,0.3,True)[1]
-    sum_counts[9] = fn.stats(counts6,0.25,True)[1]
-    sum_counts[10] = fn.stats(counts69h,0.248,True)[1]
-    sum_counts[11] = fn.stats(counts7,0.2,True)[1]
-    sum_counts[12] = fn.stats(counts8,0.15,True)[1]
-    sum_counts[13] = fn.stats(counts84h,0.128,True)[1]
-    sum_counts[14] = fn.stats(counts9,0.1,True)[1]
-    sum_counts[15] = fn.stats(counts98h,0.016,True)[1]
-    sum_counts[16] = fn.stats(counts10,0.05,True)[1]
+    sum_counts[0] = fn.stats(counts1,0.7,True)[1]
+    sum_counts[1] = fn.stats(counts2,0.6,True)[1]
+    sum_counts[2] = fn.stats(counts3,0.5,True)[1]
+    sum_counts[3] = fn.stats(counts4,0.4,True)[1]
+    sum_counts[4] = fn.stats(counts5,0.3,True)[1]
+    sum_counts[5] = fn.stats(counts6,0.25,True)[1]
+    sum_counts[6] = fn.stats(counts7,0.2,True)[1]
+    sum_counts[7] = fn.stats(counts8,0.15,True)[1]
+    sum_counts[8] = fn.stats(counts9,0.1,True)[1]
+    sum_counts[9] = fn.stats(counts10,0.05,True)[1]
     
     #%% Radiation Sheilding Plot
     #This plots Counts vs Distance from the overall count and its respecitve distance. 
@@ -124,8 +94,8 @@ if __name__ == "__main__":
     plt.xlabel("Distance (m)")
     plt.ylabel("Counts")
     plt.scatter(distance, sum_counts,label='Shielding Trend')
-    x = np.linspace(0.05,1,1000)
-    plt.plot(x,16/((x**2)), label='Inverse Square')
+    #x = np.linspace(0.05,1,1000)
+    #plt.plot(x,16/((x**2)), label='Inverse Square')
     plt.legend()
     plt.grid()
     plt.show()
@@ -144,13 +114,6 @@ if __name__ == "__main__":
     fn.plot_counts(time8,counts8,0.15)
     fn.plot_counts(time9,counts9,0.1)
     fn.plot_counts(time10,counts10,0.05)
-    fn.plot_counts(time1h,counts1h,0.792)
-    fn.plot_counts(time19h,counts19h,0.648)
-    fn.plot_counts(time37h,counts37h,0.504)
-    fn.plot_counts(time55h,counts55h,0.36)
-    fn.plot_counts(time69h,counts69h,0.248)
-    fn.plot_counts(time84h,counts84h,0.128)
-    fn.plot_counts(time98h,counts98h,0.016)
 
     #%% Calculating the Intensity of Radioactive Decay
     
@@ -160,7 +123,7 @@ if __name__ == "__main__":
     #We first start off by defining an array to store counts per second values, and then calcualte the rate of decay and store 
     #into the array.
 
-    intensity_array = np.zeros(17)
+    intensity_array = np.zeros(10)
     int1 = sum(counts1) / sum(time1)
     int2 = sum(counts2) / sum(time2)
     int3 = sum(counts3) / sum(time3)
@@ -171,13 +134,6 @@ if __name__ == "__main__":
     int8 = sum(counts8) / sum(time8)
     int9 = sum(counts9) / sum(time9)
     int10 = sum(counts10) / sum(time10)
-    int11 = sum(counts1h)/sum(time1h)
-    int12 = sum(counts19h)/sum(time19h)
-    int13 = sum(counts37h)/sum(time37h)
-    int14 = sum(counts55h)/sum(time55h)
-    int15 = sum(counts69h)/sum(time69h)
-    int16 = sum(counts84h)/sum(time84h)
-    int17 = sum(counts98h)/sum(time98h)
 
     #These two parts are really the same parts of the code, we define the Intensity array, based on multiple divisions.
     #In order to accomodate the machine error, we truncated much of the decimal places when storing into the array.
@@ -192,14 +148,7 @@ if __name__ == "__main__":
     intensity_array[7] = float(f"{int8:.4f}")
     intensity_array[8] = float(f"{int9:.4f}")
     intensity_array[9] = float(f"{int10:.6f}")
-    intensity_array[10] = float(f"{int11:.6f}")
-    intensity_array[11] = int12
-    intensity_array[12] = int13
-    intensity_array[13] = int14
-    intensity_array[14] = int15
-    intensity_array[15] = int16
-    intensity_array[16] = int17
-    
+
     #%% Intensity vs Inverse Square Fitting
     #In order to obtain our constant, S, we defined the inverse_sqr variable using the data.
     inverse_sqr = 1 / (distance**2)
@@ -211,7 +160,7 @@ if __name__ == "__main__":
     plt.figure(2)
     plt.figure(figsize=(7,5))
     plt.scatter(intensity_array, inverse_sqr, label='Strength')
-    #plt.plot(intensity_array, fn.lin_curve(intensity_array, int_fit[1],int_fit[0]), label='Fitted Curve')
+    plt.plot(intensity_array, fn.lin_curve(intensity_array, int_fit[1],int_fit[0]), label='Fitted Curve')
     plt.title("Plotting Intensity vs Inverse Square")
     plt.xlabel("Intensity (C/s)")
     plt.ylabel("1 / r^2")
@@ -227,24 +176,25 @@ if __name__ == "__main__":
     C = intensity_array
     
     #After obbtaining our values, we then define an array to store the area calculations. 
-    areal = np.zeros(17)
-    #Then, for the first 10 values, which are the farthest away, we input our 1/12 method into calculating the area
+    areal = np.zeros(10)
+    #Then, for the first 3 values, which are the farthest away, we input our 1/180 method into calculating the area
     #And then we truncate to 6 or 8 decimal places depending on the distance. 
     
-    for kj in range(10):
-        areal[kj] = (C[kj]) *(distance[kj]**2) / 12
+    for kj in range(3):
+        areal[kj] = (C[kj]) *(distance[kj]**2) / 180
         areal[kj] = float(f'{areal[kj]:.6f}')
         
-    for kj in range(10,17):
-        areal[kj] = (C[kj]) * (distance[kj]**2) / 3
+    for kj in range(3,10):
+        areal[kj] = (C[kj]) * (distance[kj]**2) / 36
         areal[kj] = float(f'{areal[kj]:.8f}')
         
     #After all of the values are stored, we then multiply into the array all of the constant values.
     #We also determine the uncertainty from the standard deviation
-    areal *= (4*np.pi)/S
+    
+    areal *= (4*np.pi)/(S)
     d_error = np.std(areal)
     
     #These prints then print the final results. 
-    print(f"The Approximate Area of the Detector is: {((sum(areal))*(10000)):.3f} cm^2")
+    print(f"The Approximate Area of the Detector is: {((np.average(areal))*(10000)):.3f} cm^2")
     print(f"The error in this approximation is: +/- {(d_error*10000):.2f} cm^2")
     
